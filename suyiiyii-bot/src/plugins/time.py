c925ddecb26d5bfb5af71a59c96ae1f2825a2b30
suyiiyii <suyiiyii@gmail.com>
2024-07-16 17:20:34 +0800

    saa主动发消息

--- suyiiyii-bot/src/plugins/time.py
+++ suyiiyii-bot/src/plugins/time.py
@@ -1,13 +1,25 @@
 import time
-from nonebot import on_command
+from nonebot import require, on_command, get_bot
 from nonebot.rule import to_me
-# from nonebot.adapters import Message, MessageSegment
-from nonebot.adapters.console import Message, MessageSegment
+from nonebot.adapters import Message, MessageSegment
+
+# from nonebot.adapters.console import Message, MessageSegment
 from nonebot.params import CommandArg
+
+require("nonebot_plugin_saa")
+from nonebot_plugin_saa import Text
+
 
 check_time = on_command(
     "time", rule=to_me, aliases={"t", "时间", "几点了"}, priority=5, block=True
 )
+
+
+async def send_private():
+    from nonebot_plugin_saa import TargetQQPrivate
+
+    target = TargetQQPrivate(user_id=1462845368)
+    await Text("博士，你今天有早八，还不能休息哦").send_to(target, get_bot())
 
 
 @check_time.handle()
@@ -16,12 +28,6 @@ async def handle(args: Message = CommandArg()):
     replys.append(f"现在时间是：{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
     replys.append(f"命令参数是：{args}")
 
-    message = Message(
-        [
-            MessageSegment(type="text", data={"text": "hello"}),
-            MessageSegment(type="markdown", data={"markup": "**world**"}),
-        ]
-    )
-
-    # await check_time.finish("\n".join(replys))
-    await check_time.finish(message)
+    text = Text("\n".join(replys))
+    await send_private()
+    await text.send(at_sender=True, reply=True)
